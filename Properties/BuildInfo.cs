ab0da779dd3daca066c6f9fc6d5340fb55a4f08f
Herp Derpinstine <herpderpinstine@gmail.com>
2022-04-05 01:34:46 -0500

    Bumped Version

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -5,7 +5,7 @@
         public const string Name = "bHapticsOSC";
         public const string Author = "Herp Derpinstine";
         public const string Company = "Lava Gang";
-        public const string Version = "2.0s.0";
+        public const string Version = "2.0.0";
         public const string DownloadLink = "https://github.com/HerpDerpinstine/bHapticsOSC";
     }
 }
