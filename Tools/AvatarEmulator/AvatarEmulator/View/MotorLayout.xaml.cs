8ea27d166aad44be15344ef05fe522fd99ef28ac
Sangwon Choi <sangwon.choi@bhaptics.com>
2022-04-12 21:23:34 +0900

    Add emulator

---
+++ Tools/AvatarEmulator/AvatarEmulator/View/MotorLayout.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AvatarEmulator.View
+{
+    public partial class MotorLayout : UserControl
+    {
+        public MotorLayout()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Tools/AvatarEmulator/AvatarEmulator/View/SettingView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace AvatarEmulator.View
+{
+    public partial class SettingView : UserControl
+    {
+        public SettingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
